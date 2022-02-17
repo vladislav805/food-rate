c24f97a1a7cd585a9fd11506f1c093752a641e39
Vladislav Veluga <vladislav805@yandex.com>
2022-02-17 22:51:15 +0300

    fix: Исправление подложки-модалки в гейфоне

--- src/frontend/utils/renderFullPage.ts
+++ src/frontend/utils/renderFullPage.ts
@@ -14,7 +14,7 @@ export const renderFullPage = (markup: string, meta: IFullPageOptions): string =
 <html lang="ru">
     <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes" />
+        <meta name="viewport" content="initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover" />
         <title>${documentTitle}</title>
         <link rel="stylesheet" href="/static/styles.css" />
     </head>

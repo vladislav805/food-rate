a75cd781b8232a14756c381ae5acb1ede017999c
Vladislav Veluga <vladislav805@yandex.com>
2022-02-19 15:52:11 +0300

    chore: Перенос кода авторизации ВК из /external/vk в /frontend/external/authorize

--- src/frontend/external/authorize/vk.ts
+++ src/frontend/external/authorize/vk.ts
@@ -1,8 +1,35 @@
+import axios from 'axios';
+import config from '%config';
+
 import AuthorizeService from './AuthorizeService';
 import type { IUserInfo, AuthorizationServiceName } from './typings';
-import VkClient from '@external/vk/client';
-import VkServer from '@external/vk/server';
-import type { IVkAuthorizationResult } from '@external/vk/typings';
+
+export interface IVkAuthorizationResult {
+    access_token: string;
+    expires_in: number;
+    user_id: number;
+}
+
+export interface IVkResponse<T> {
+    response: T;
+}
+
+export interface IVkError {
+    error: {
+        error_code: number;
+        error_msg: string;
+    };
+}
+
+export type IVkApiResult<T> = IVkResponse<T> | IVkError;
+
+export interface IVkUser {
+    id: number;
+    first_name: string;
+    last_name: string;
+    photo_100: string;
+    photo_200: string;
+}
 
 export default class AuthorizeServiceVK extends AuthorizeService {
     serviceName: AuthorizationServiceName = 'vk';
@@ -10,7 +37,16 @@ export default class AuthorizeServiceVK extends AuthorizeService {
     protected auth?: IVkAuthorizationResult;
 
     public override getAuthorizeUrl(): string {
-        return VkClient.getAuthorizationUrl();
+        const params = new URLSearchParams({
+            display: 'page',
+            client_id: config.VK_APP_ID,
+            redirect_uri: config.AUTH_URL_VK_REDIRECT,
+            scope: 'offline',
+            response_type: 'code',
+            v: config.VK_API_VERSION,
+        });
+
+        return `https://oauth.vk.com/authorize?${params.toString()}`;
     }
 
     public override isEmptyRequest(): boolean {
@@ -18,14 +54,58 @@ export default class AuthorizeServiceVK extends AuthorizeService {
     }
 
     public override async getServiceUserId(): Promise<string> {
-        this.auth = await VkServer.getAuthorizationInfoByCode(this.request.query.code as string);
-        return String(this.auth.user_id);
+        const code = this.request.query.code as string;
+
+        const params = new URLSearchParams({
+            client_id: config.VK_APP_ID,
+            client_secret: config.VK_APP_SECRET,
+            redirect_uri: config.AUTH_URL_VK_REDIRECT,
+            code,
+        });
+
+        const url = `https://oauth.vk.com/access_token?${params.toString()}`;
+
+        const { status, data } = await axios.get<IVkAuthorizationResult>(url, {
+            responseType: 'json',
+            validateStatus: () => true,
+        });
+
+        if (status !== 200 || !('access_token' in data)) {
+            throw new Error(`Unknown error: ${status}`);
+        }
+
+        this.auth = data;
+
+        return String(data.user_id);
     }
 
     public override async getUserInfo(): Promise<IUserInfo> {
         if (!this.auth) throw new Error('No auth');
 
-        const user = await VkServer.getUserInfo(this.auth.access_token);
+        const params = new URLSearchParams({
+            access_token: this.auth.access_token,
+            v: config.VK_API_VERSION,
+            fields: 'photo_100,photo_200'
+        });
+
+        const url = `https://api.vk.com/method/users.get?${params.toString()}`;
+
+        const { status, data } = await axios.get<IVkApiResult<IVkUser[]>>(url, {
+            responseType: 'json',
+            validateStatus: () => true,
+        });
+
+        if (status !== 200 || 'error' in data) {
+            throw new Error((data as IVkError).error.error_msg);
+        }
+
+        const { response } = data;
+
+        if (!Array.isArray(response) || !response.length) {
+            throw new Error('Unknown error (no user)');
+        }
+
+        const [user] = response;
 
         return {
             service: 'vk',
